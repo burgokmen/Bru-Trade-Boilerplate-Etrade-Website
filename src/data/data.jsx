0844594fa4d51147ad599a6d34b80c16135d088a
burgokmen <burgokmen@gmail.com>
2023-09-26 22:45:09 +0300

    headertop icons added/ dynamic data created

--- src/data/data.jsx
+++ src/data/data.jsx
@@ -1,6 +1,6 @@
 const data = {
   myStore: {
-    phoneNumber: "(510) 999-0718",
+    phoneNumber: "(510) 999 07 18",
     email: "brunogokmen@gmail.com",
     discountLetter: "Follow Us and get a chance to win 25% off",
   },
