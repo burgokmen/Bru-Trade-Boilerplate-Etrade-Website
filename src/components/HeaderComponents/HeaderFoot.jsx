b888079725f7d1e6b752d183996ce44474da0103
burgokmen <burgokmen@gmail.com>
2023-09-28 00:08:48 +0300

    header updated with dynamic data

--- src/components/HeaderComponents/HeaderFoot.jsx
+++ src/components/HeaderComponents/HeaderFoot.jsx
@@ -1,10 +1,11 @@
+import data from "../../data/data";
 import HeaderComplex from "./HeaderComplex";
 import NavBar from "./NavBar";
 
 const HeaderFoot = () => {
   return (
     <div className="flex px-[1.5rem] justify-between">
-      <h1 className="text-[24px] text-[#252B42]">Bru7</h1>
+      <h1 className="text-[24px] text-[#252B42]">{data.brand.name}</h1>
       <NavBar />
       <HeaderComplex />
     </div>
