c97fc2a072ad6f8955b187c7b9bb69b119ed97f3
burgokmen <burgokmen@gmail.com>
2023-09-29 01:28:26 +0300

    Product header done

--- src/components/ProductListPageComponents/ProductHeader.jsx
+++ src/components/ProductListPageComponents/ProductHeader.jsx
@@ -1,12 +1,12 @@
 const ProductHeader = () => {
   return (
-    <div className="bg-[#FAFAFA]">
+    <div className="bg-[#FAFAFA] py-[24px] text-center">
       <div className="flex justify-between px-10">
-        <h2>Shop</h2>
-        <div className="flex">
-          <h3>Home</h3>
-          <i className="fa fa-chevron-right"></i>
-          <h4>Shop</h4>
+        <h2 className="text-[24px] text-darkBlue">Shop</h2>
+        <div className="flex justify-between">
+          <h3 className="text-[14px] text-darkBlue pr-3">Home</h3>
+          <i className="fa fa-chevron-right pr-3 pt-1 text-[#BDBDBD]"></i>
+          <h4 className="text-[14px] text-[#BDBDBD]">Shop</h4>
         </div>
       </div>
     </div>
