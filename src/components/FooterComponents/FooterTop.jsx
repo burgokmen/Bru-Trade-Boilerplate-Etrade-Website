5667c2789796804735b41206d9c20019ce9773d7
burgokmen <burgokmen@gmail.com>
2023-09-28 23:41:28 +0300

    footer icons added

--- src/components/FooterComponents/FooterTop.jsx
+++ src/components/FooterComponents/FooterTop.jsx
@@ -1,20 +1,13 @@
 import data from "../../data/data";
-import faceIconSvg from "../../assets/header-top-icons/face-icon.svg";
-import instaIconSvg from "../../assets/header-top-icons/insta-icon.svg";
-import twitterIconSvg from "../../assets/header-top-icons/twitter-icon.svg";
 
 const FooterTop = () => {
   return (
     <div className="flex h-[142px] px-[195px] py-[40px] justify-between items-center bg-[#FAFAFA]">
       <h1 className="text-[24px] text-[#252B42]">{data.brand.name}</h1>
-      <div className="flex p-[10px] ">
-        <img
-          className="px-[10px] stroke-cyan-500 "
-          src={instaIconSvg}
-          alt="Phone"
-        />
-        <img className="px-[10px]" src={faceIconSvg} alt="Phone" />
-        <img className="px-[10px]" src={twitterIconSvg} alt="Phone" />
+      <div className="flex p-[10px] text-babyBlue ">
+        <i className="fa fa-instagram pr-5"></i>
+        <i className="fa fa-youtube pr-5"></i>
+        <i className="fa fa-twitter"></i>
       </div>
     </div>
   );
